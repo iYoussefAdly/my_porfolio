03a1a6411a9b18322a1a0cd77647ebe71629beb9
youssefadly <youssefadly332@gmail.com>
2025-09-10 20:20:52 +0300

    second commit

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,9 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
-  const isDev = mode === "development";
+  loadEnv(mode, process.cwd(), "");
   return {
-    base: isDev ? "/" : `/${env.VITE_REPO_NAME}/`,
+    base: "./",
     plugins: [react(), tailwindcss()],
     server: {
       open: true,
